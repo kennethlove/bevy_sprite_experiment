bd3e2ed632d2a3ef1877c321975bec37f4c05845
Kenneth Love <kennethlove@gmail.com>
2024-03-10 23:34:11 -0700

    sprite has physics. jump and idle animations aren't working

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+mod animation;
+mod platforms;
+mod player;
+
+use animation::AnimationPlugin;
+use platforms::PlatformsPlugin;
+use player::PlayerPlugin;
+
+const WINDOW_WIDTH: f32 = 1024.;
+const WINDOW_HEIGHT: f32 = 720.;
+
+pub const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.;
+const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.;
+
+const COLOR_BACKGROUND: Color = Color::DARK_GRAY;
+const COLOR_FLOOR: Color = Color::GREEN;
+
+const FLOOR_THICKNESS: f32 = 10.;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(COLOR_BACKGROUND))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Sprite Experiment".to_string(),
+                        resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        ) // prevents blurry sprites
+        .add_plugins((
+            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.),
+            RapierDebugRenderPlugin::default(),
+        ))
+        .add_plugins((PlatformsPlugin, PlayerPlugin, AnimationPlugin))
+        .add_systems(Startup, setup)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: COLOR_FLOOR,
+                ..default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0., WINDOW_BOTTOM_Y - FLOOR_THICKNESS / 2., 0.),
+                scale: Vec3::new(WINDOW_WIDTH, FLOOR_THICKNESS, 1.),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(RigidBody::Fixed)
+        .insert(Collider::cuboid(0.5, 0.5));
+}
---
+++ src/player.rs
@@ -0,0 +1,274 @@
+use std::time::Duration;
+
+use crate::animation::Animation;
+use crate::WINDOW_BOTTOM_Y;
+use crate::WINDOW_LEFT_X;
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+const SPRITESHEET_COLS: usize = 8;
+const SPRITESHEET_ROWS: usize = 9;
+const SPRITE_TILE_WIDTH: f32 = 32.;
+const SPRITE_TILE_HEIGHT: f32 = 32.;
+const SPRITE_RENDER_WIDTH: f32 = 64.;
+const SPRITE_RENDER_HEIGHT: f32 = 64.;
+const SPRITE_IDX_STAND: usize = 0;
+const SPRITE_IDX_IDLE: &[usize] = &[0, 1];
+const SPRITE_IDX_WALK: &[usize] = &[16, 17, 18, 19];
+const SPRITE_IDX_JUMP: &[usize] = &[40, 41, 42, 43, 44, 45, 46, 47];
+const IDLE_CYCLE_DELAY: Duration = Duration::from_millis(90);
+const WALK_CYCLE_DELAY: Duration = Duration::from_millis(70);
+const JUMP_CYCLE_DELAY: Duration = Duration::from_millis(70);
+const PLAYER_VELOCITY_X: f32 = 400.;
+const PLAYER_VELOCITY_Y: f32 = 850.;
+
+const MAX_JUMP_HEIGHT: f32 = 230.;
+
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Component)]
+struct Jump(f32);
+
+#[derive(Component)]
+enum Direction {
+    Left,
+    Right,
+}
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup).add_systems(
+            Update,
+            (
+                movement,
+                jump,
+                rise,
+                fall,
+                apply_movement_animation,
+                apply_idle_sprite,
+                apply_jump_sprite,
+                update_direction,
+                update_sprite_direction,
+            ),
+        );
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut atlases: ResMut<Assets<TextureAtlasLayout>>,
+    asset_server: Res<AssetServer>,
+) {
+    let image_handle: Handle<Image> = asset_server.load("AnimationSheet_Character.png");
+    let texture_atlas = TextureAtlasLayout::from_grid(
+        Vec2::new(SPRITE_TILE_WIDTH, SPRITE_TILE_HEIGHT),
+        SPRITESHEET_COLS,
+        SPRITESHEET_ROWS,
+        None,
+        None,
+    );
+    let atlas_handle = atlases.add(texture_atlas);
+
+    commands
+        .spawn(SpriteSheetBundle {
+            sprite: Sprite::default(),
+            atlas: TextureAtlas {
+                layout: atlas_handle,
+                index: SPRITE_IDX_STAND,
+            },
+            texture: image_handle,
+            transform: Transform {
+                translation: Vec3::new(WINDOW_LEFT_X + 100., WINDOW_BOTTOM_Y + 300., 0.),
+                scale: Vec3::new(
+                    SPRITE_RENDER_WIDTH / SPRITE_TILE_WIDTH,
+                    SPRITE_RENDER_HEIGHT / SPRITE_TILE_HEIGHT,
+                    1.,
+                ),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Collider::cuboid(
+            SPRITE_TILE_WIDTH / 2.,
+            SPRITE_TILE_HEIGHT / 2.,
+        ))
+        .insert(KinematicCharacterController::default())
+        .insert(Direction::Right);
+}
+
+fn movement(
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut KinematicCharacterController>,
+) {
+    let mut player = query.single_mut();
+
+    let mut movement = 0.0;
+
+    if input.pressed(KeyCode::ArrowRight) {
+        movement += time.delta_seconds() * PLAYER_VELOCITY_X;
+    }
+    if input.pressed(KeyCode::ArrowLeft) {
+        movement -= time.delta_seconds() * PLAYER_VELOCITY_X;
+    }
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(movement, vec.y)),
+        None => player.translation = Some(Vec2::new(movement, 0.)),
+    }
+}
+
+fn jump(
+    mut commands: Commands,
+    input: Res<ButtonInput<KeyCode>>,
+    query: Query<
+        (Entity, &KinematicCharacterControllerOutput),
+        (With<KinematicCharacterController>, Without<Jump>),
+    >,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output) = query.single();
+
+    if input.pressed(KeyCode::ArrowUp) && output.grounded {
+        commands.entity(player).insert(Jump(0.));
+    }
+}
+
+fn rise(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut KinematicCharacterController, &mut Jump)>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (entity, mut player, mut jump) = query.single_mut();
+
+    let mut movement = time.delta().as_secs_f32() * PLAYER_VELOCITY_Y;
+
+    if movement + jump.0 >= MAX_JUMP_HEIGHT {
+        movement = MAX_JUMP_HEIGHT - jump.0;
+        commands.entity(entity).remove::<Jump>();
+    }
+
+    jump.0 += movement;
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
+        None => player.translation = Some(Vec2::new(0.0, movement)),
+    }
+}
+
+fn fall(time: Res<Time>, mut query: Query<&mut KinematicCharacterController, Without<Jump>>) {
+    if query.is_empty() {
+        return;
+    }
+
+    let mut player = query.single_mut();
+
+    let movement = time.delta().as_secs_f32() * (PLAYER_VELOCITY_Y / 1.5) * -1.;
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
+        None => player.translation = Some(Vec2::new(0.0, movement)),
+    }
+}
+
+fn apply_movement_animation(
+    mut commands: Commands,
+    query: Query<(Entity, &KinematicCharacterControllerOutput), Without<Animation>>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output) = query.single();
+    if output.desired_translation.x != 0.0 && output.grounded {
+        commands
+            .entity(player)
+            .insert(Animation::new(SPRITE_IDX_WALK, WALK_CYCLE_DELAY));
+    }
+}
+
+fn apply_idle_sprite(
+    mut commands: Commands,
+    mut query: Query<(
+        Entity,
+        &KinematicCharacterControllerOutput,
+        &mut TextureAtlas,
+    )>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output, mut sprite) = query.single_mut();
+    if output.desired_translation.x == 0.0 && output.grounded {
+        commands
+            .entity(player)
+            .insert(Animation::new(SPRITE_IDX_IDLE, IDLE_CYCLE_DELAY));
+        // sprite.index = SPRITE_IDX_IDLE[0];
+    }
+}
+
+fn apply_jump_sprite(
+    mut commands: Commands,
+    mut query: Query<(
+        Entity,
+        &KinematicCharacterControllerOutput,
+        &mut TextureAtlas,
+    )>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output, mut sprite) = query.single_mut();
+    if !output.grounded {
+        commands
+            .entity(player)
+            .insert(Animation::new(SPRITE_IDX_JUMP, JUMP_CYCLE_DELAY));
+        // sprite.index = SPRITE_IDX_JUMP[0];
+    }
+}
+
+fn update_direction(
+    mut commands: Commands,
+    query: Query<(Entity, &KinematicCharacterControllerOutput)>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output) = query.single();
+
+    if output.desired_translation.x > 0. {
+        commands.entity(player).insert(Direction::Right);
+    } else if output.desired_translation.x < 0. {
+        commands.entity(player).insert(Direction::Left);
+    }
+}
+
+fn update_sprite_direction(mut query: Query<(&mut Sprite, &Direction)>) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (mut sprite, direction) = query.single_mut();
+    match direction {
+        Direction::Right => {
+            sprite.flip_x = false;
+        }
+        Direction::Left => {
+            sprite.flip_x = true;
+        }
+    }
+}
